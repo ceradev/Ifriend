badb0965148ac53b7667315e5fa4d3f1c68ae0f7
César Suárez <cesararamissuarez@gmail.com>
2024-01-12 17:06:40 +0000

    The files are sent

--- src/views/default/index.blade.php
+++ src/views/default/index.blade.php
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
-  <h1>{{$name}}</h1>
-</body>
-</html>
+@extends('layouts.master')
+
+@section('title', 'Default Title')
+
+@section('subtitle', 'Default Subtitle')
+
+@section('container')
+
+
+<p>Bienvenido {{$name}}</p>
+
+@endsection
--- src/views/layouts/master.blade.php
+++ src/views/layouts/master.blade.php
@@ -4,9 +4,18 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>@yield('title')</title>
 </head>
 <body>
-  <h1>Bienvenido {{$name}}</h1>
+
+  <h1>Titulo de la aplicación</h1>
+
+  <h2>@yield('subtitle')</h2>
+
+  <main>
+
+  @yield('container')
+
+</main>
 </body>
 </html>
