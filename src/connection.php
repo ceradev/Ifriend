7e5665386cf1d8ec88f8a449d1cbabdc8bca7d6d
César Suárez <cesararamissuarez@gmail.com>
2024-01-16 19:02:10 +0000

    The files are sent

--- src/connection.php
+++ src/connection.php
@@ -7,10 +7,10 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 $capsule = new Capsule;
 $capsule->addConnection([
   'driver'    => 'mysql',
-  'host'      => 'localhost',
-  'database'  => 'ifriend',
-  'username'  => 'root',
-  'password'  => '',
+  'host'      => $_ENV['DB_HOST'],
+  'database'  => $_ENV['DB_NAME'],
+  'username'  => $_ENV['DB_USER'],
+  'password'  => $_ENV['DB_PASSWORD'],
   'charset'   => 'utf8mb4',
   'collation' => 'utf8mb4_unicode_ci',
   'prefix'    => '',
