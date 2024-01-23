26cd3c59941377ca00e2d95d4c5168e4e7ec0236
Victor <evilpip1@protonmail.com>
2024-01-23 20:15:10 +0000

    .

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -2,13 +2,16 @@
 
 use Dsw\Ifriend\Controllers\LoginController;
 
+$router->map('GET', '/', 'DefaultController#index', 'index');
 $router->map('POST', '/login', 'LoginController#validate', 'login');
 $router->map('GET', '/logout', 'LoginController#logout', 'logout');
 
-$router->map('GET', '/', 'DefaultController#index', 'index');
-$router->map('GET', '/users/list', 'UserController#list', 'user_list');
-$router->map('GET', '/users/[i:id]/delete', 'UserController#delete', 'user_delete');
-$router->map('GET', '/users/create', 'UserController#create', 'user_create');
-$router->map('POST', '/users', 'UserController#post', 'user_post');
-$router->map('GET', '/users/[i:id]', 'UserController#edit', 'user_edit');
-$router->map('POST', '/users/[i:id]', 'UserController#update', 'user_update');
+
+if (isset($_SESSION['id'])) {
+  $router->map('GET', '/users/list', 'UserController#list', 'user_list');
+  $router->map('GET', '/users/[i:id]/delete', 'UserController#delete', 'user_delete');
+  $router->map('GET', '/users/create', 'UserController#create', 'user_create');
+  $router->map('POST', '/users', 'UserController#post', 'user_post');
+  $router->map('GET', '/users/[i:id]', 'UserController#edit', 'user_edit');
+  $router->map('POST', '/users/[i:id]', 'UserController#update', 'user_update');
+}
--- src/views/layouts/header.blade.php
+++ src/views/layouts/header.blade.php
@@ -9,9 +9,11 @@
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="{{$router->generate('index')}}">Home</a>
         </li>
+        @isset($_SESSION['id'])
         <li class="nav-item">
           <a class="nav-link" href="{{$router->generate('user_list')}}">Users</a>
         </li>
+        @endisset
         <li class="nav-item">
           <a class="nav-link disabled" aria-disabled="true">Disabled</a>
         </li>
